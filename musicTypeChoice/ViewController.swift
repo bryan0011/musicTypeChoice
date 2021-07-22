46f34706362e00aa76ede23b8fb33f7482d16160
Bryan Kuo <bryan0011@gmail.com>
2021-07-22 15:21:21 +0800

    Initial Commit

---
+++ musicTypeChoice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musicTypeChoice
+//
+//  Created by Bryan Kuo on 2021/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
